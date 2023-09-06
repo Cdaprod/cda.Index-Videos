5924d76b091496fe59a4fa8b75fc34b752c53ae4
David <104325852+Cdaprod@users.noreply.github.com>
2023-09-06 13:38:38 -0400

    Added database updating, embedding calculation, main, metadata generation, and video processing files

---
+++ Guided_Notebook/Flask_App/main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ Guided_Notebook/Flask_App/metadata_generation.py
@@ -0,0 +1,2 @@
+#!/usr/bin/env python3
+# TODO: Add your metadata generation code here
